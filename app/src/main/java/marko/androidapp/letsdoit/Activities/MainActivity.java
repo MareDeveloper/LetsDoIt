9785cf40e588fa7b3178c95594486004916aee89
MareDeveloper <markodjokic886@gmail.com>
2020-08-26 22:13:34 +0200

    Create new method for popup dialog 	new file:   .idea/compiler.xml 	modified:   app/src/main/java/marko/androidapp/letsdoit/Activities/MainActivity.java

--- app/src/main/java/marko/androidapp/letsdoit/Activities/MainActivity.java
+++ app/src/main/java/marko/androidapp/letsdoit/Activities/MainActivity.java
@@ -2,6 +2,9 @@ package marko.androidapp.letsdoit.Activities;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,44 +18,60 @@ import android.view.MenuItem;
 
 import marko.androidapp.letsdoit.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog builder;
+    private EditText title;
+    private EditText description;
+    private Button save;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopUpDialog();
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createPopUpDialog()
+    {
+
     }
 }
