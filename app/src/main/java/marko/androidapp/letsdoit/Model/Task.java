2ecade41f9a344e8b0b1645774c24e295b7937e6
MareDeveloper <markodjokic886@gmail.com>
2020-08-27 21:43:17 +0200

    New class created 	new file:   app/src/main/java/marko/androidapp/letsdoit/Model/Task.java

---
+++ app/src/main/java/marko/androidapp/letsdoit/Model/Task.java
@@ -0,0 +1,10 @@
+package marko.androidapp.letsdoit.Model;
+
+public class Task
+{
+    private String name;
+    private String description;
+    private String dateItemAdded;
+    private String DoDate;
+    private int id;
+}
