9ee05dc388e25ed25ae4f918bc197444a73d63e4
MareDeveloper <markodjokic886@gmail.com>
2020-08-29 19:53:44 +0200

    Created Task class constructor, getter and setter 	modified:   app/src/main/java/marko/androidapp/letsdoit/Model/Task.java

--- app/src/main/java/marko/androidapp/letsdoit/Model/Task.java
+++ app/src/main/java/marko/androidapp/letsdoit/Model/Task.java
@@ -7,4 +7,58 @@ public class Task
     private String dateItemAdded;
     private String DoDate;
     private int id;
+
+    public Task()
+    {
+
+    }
+
+    public Task(String name, String description, String dateItemAdded, String doDate, int id)
+    {
+        this.name = name;
+        this.description = description;
+        this.dateItemAdded = dateItemAdded;
+        this.DoDate = doDate;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateItemAdded() {
+        return dateItemAdded;
+    }
+
+    public void setDateItemAdded(String dateItemAdded) {
+        this.dateItemAdded = dateItemAdded;
+    }
+
+    public String getDoDate() {
+        return DoDate;
+    }
+
+    public void setDoDate(String doDate) {
+        DoDate = doDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
